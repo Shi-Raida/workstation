c247649e8e2e0cacfbd2c8500b44fb8151361252
Roland Godet <r.godet@outlook.fr>
2023-10-25 21:56:50 +0200

    fix(run): handle empty passwords

--- run.sh
+++ run.sh
@@ -11,13 +11,13 @@ file_path="$(readlink -f "$file")"
 cd "${0%/*}" || exit
 command -v ansible > /dev/null 2>&1 || sudo ./scripts/bootstrap-ansible.sh
 
-if [[ ! -f "$become_password_file" ]]; then
+if [ ! -f "$become_password_file" -o ! -s "$become_password_file" ]; then
   read -rs -p 'become password: ' become_password
   echo
   printf '%s' "$become_password" > "$become_password_file"
 fi
 
-if [[ ! -f "$vault_password_file" ]]; then
+if [ ! -f "$vault_password_file" -o ! -s "$vault_password_file" ]; then
   read -rs -p 'vault password: ' vault_password
   echo
   printf '%s' "$vault_password" > "$vault_password_file"
