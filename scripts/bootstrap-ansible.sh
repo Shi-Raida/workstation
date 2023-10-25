a84ef19104bd7f329a2b4d841212d396ea35f332
Roland Godet <r.godet@outlook.fr>
2023-10-25 21:50:33 +0200

    fix(scripts): ansible installation directories

--- scripts/bootstrap-ansible.sh
+++ scripts/bootstrap-ansible.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-opt_dir="$HOME/.local/opt"
-bin_dir="$HOME/.local/bin"
+opt_dir="/usr/local/opt"
+bin_dir="/usr/local/bin"
 
-dpkg -s python3-venv > /dev/null 2>&1 || sudo apt install -y python3-venv
+dpkg -s python3-venv > /dev/null 2>&1 || apt install -y python3-venv
 python3 -m venv "$opt_dir/ansible"
 . "$opt_dir/ansible/bin/activate" || exit
 pip install ansible
