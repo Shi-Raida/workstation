cd946b78d8fadc209f65871d258a9cc119778dcc
Roland Godet <r.godet@outlook.fr>
2023-11-19 20:09:53 +0100

    fix(power-menu): icons

--- files/i3/power-menu.sh
+++ files/i3/power-menu.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-MAP=" Lock screen,i3lock-fancy
- Log out,i3-msg exit
-⏾ Suspend,i3lock-fancy & systemctl suspend
- Hibernate,i3lock-fancy & systemctl hibernate
- Reboot,systemctl reboot
- Shut down,systemctl poweroff -i"
+MAP="  Lock screen,i3lock-fancy
+  Log out,i3-msg exit
+⏾  Suspend,i3lock-fancy & systemctl suspend
+  Hibernate,i3lock-fancy & systemctl hibernate
+  Reboot,systemctl reboot
+  Shut down,systemctl poweroff -i"
 
 selection=$(echo "$MAP" \
     | cut -d ',' -f 1 \
-    | rofi -dmenu -p "Power menu " -l 6 -no-custom -i -m -1 \
+    | rofi -dmenu -p "Power menu " -l 6 -no-custom -i -m -1 -theme-str 'window {width: env(WIDTH, 60ch);}' \
     | head -n 1)
 
 if [ -n "$selection" ]; then
